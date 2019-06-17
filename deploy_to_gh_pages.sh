e13e061b849a1dd89c2e2051ccb6d37f550d12c0
fang duan <df1228@gmail.com>
2019-06-17 17:44:06 +0800

    Update deploy script https://gohugo.io/hosting-and-deployment/hosting-on-github/#deployment-of-project-pages-from-your-gh-pages-branch

--- deploy_to_gh_pages.sh
+++
@@ -1,4 +0,0 @@
-git submodule update
-hugo --baseURL=/blog/
-cd public && git init && git add --all && git commit -m "Publishing to gh-pages with hugo --baseURL=/blog/" && cd ..
-git push origin gh-pages
